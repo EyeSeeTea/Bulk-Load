4ded0b2c9ecf8b93f7074047d74b828fa4a7b873
gqcorneby <185756521+gqcorneby@users.noreply.github.com>
2025-05-27 12:28:21 +0800

    refactor: update Maybe to | undefined. Old Maybe to NullableMaybe

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,6 +1,8 @@
 export type NonNullableValues<Obj> = { [K in keyof Obj]: NonNullable<Obj[K]> };
 
-export type Maybe<T> = T | undefined | null;
+export type Maybe<T> = T | undefined;
+
+export type MaybeNullable<T> = Maybe<T> | null;
 
 export type Dictionary<T> = Record<string, T>;
 
